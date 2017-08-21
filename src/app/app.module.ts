093049f1549ce2657a9e758e431d033dd61a3c0c
Guilherme Albuquerque <guilhermealbuquerquerc@gmail.com>
2017-08-21 16:20:42 -0300

    Ionic-crud width tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,46 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ListaCarroPage } from "../pages/lista-carro/lista-carro";
+
+export const firebaseConfig = {
+    
+    apiKey: "AIzaSyCp2xAuHDurIxjnAKa2el8tqW6nQDpY-vY",
+    authDomain: "ionic-crud-d1f7c.firebaseapp.com",
+    databaseURL: "https://ionic-crud-d1f7c.firebaseio.com",
+    projectId: "ionic-crud-d1f7c",
+    storageBucket: "ionic-crud-d1f7c.appspot.com",
+    messagingSenderId: "657981561547"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ListaCarroPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    //Inicializando Angularfire no Ionic
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ListaCarroPage
   ],
   providers: [
     StatusBar,
